6135a090db8fad8c7fb654c1e832e16e3187682c
zhenwei <zhenwei.tw@gmail.com>
2018-07-15 00:04:40 +0800

    Reorganize test programs to modular functions
    Modular functions can be reused as benchmarks to measure performance of
    IRI.

--- tests/send_transfer.py
+++ tests/send_transfer.py
@@ -1,45 +1,16 @@
 #!/usr/bin/env python
-import time
-import json
-
-import urllib.request
-import urllib.parse
-
-HOST = 'http://127.0.0.1'
-PORT = 8000
+from service import send_transfer
 
 TIPS_TYPE_IRI_REQULAR_ALGORITHM = 0
 TIPS_TYPE_TWO_NULL_IOTA_TNX = 1
+
+HOST = 'http://127.0.0.1'
+PORT = 8000
+url = str(HOST) + ":" + str(PORT)
 
 tag = "PYTHONTEST"
 message = "iHELLO"
 address = "BXEOYAONFPBGKEUQZDUZZZODHWJDWHEOYY9AENYF9VNLXZHXBOODCOTYXW9MGGINTEJPLK9AGOPTPODVX"
 value = 0
 
-url = str(HOST) + ":" + str(PORT)
-
-data = {
-    'command': 'send_transfer',
-    'tag': tag,
-    'message': message,
-    'address': address,
-    'value': value,
-    'tips_type': TIPS_TYPE_IRI_REQULAR_ALGORITHM,
-    'debug': 1
-}
-
-
-start = time.time()
-
-print("Send send transfer command ... ")
-
-req = urllib.request.Request(url)
-req.add_header('Content-Type', 'application/json')
-response = urllib.request.urlopen(req, json.dumps(data).encode('utf-8'))
-
-print("Response ... %s" % (str(response.read())))
-
-end = time.time()
-elapsed = end - start
-
-print("Duration: %s seconds" % (str(elapsed)))
+send_transfer(url, TIPS_TYPE_TWO_NULL_IOTA_TNX, tag, message, address, value)
