91f89af43032716b9e788754a2fa22567cfefb8c
yillkid <yillkid@gmail.com>
2018-08-23 02:44:45 +0800

    Add sanity checks rules

--- mk/python.mk
+++ mk/python.mk
@@ -1,24 +1,28 @@
 # check Python version
+PY_FAVORED_VER = 3.5
 PYTHON := $(shell which "python")
-PYTHON_COMPATIBLE_VERSION = 3.5
+ifndef PYTHON
+    PYTHON := $(shell which "python3")
+endif
+
+ifndef PYTHON
+    PYTHON := $(shell which "python$(PY_FAVORED_VER)")
+endif
 
 ifndef PYTHON
     $(error "Python is not executable, try to check the PATH environment variable or \
-        install python$(PYTHON_COMPATIBLE_VERSION) package.")
+        install python$(PY_FAVORED_VER) package.")
 endif # $(shell which "python")
 
-PY_CHECK_VERSION := $(shell python -c "print(str(__import__('sys').version_info.major) + \
-    str('.') + str(__import__('sys').version_info.minor) )")
+PY_CHECK_VERSION := $(shell $(PYTHON) -V)
 
-ifneq ($(PY_CHECK_VERSION), $(PYTHON_COMPATIBLE_VERSION))
-
-    PYTHON_COMPATIBLE_PATH := $(shell which "python"$(PYTHON_COMPATIBLE_VERSION))
-
-    ifdef PYTHON_COMPATIBLE_PATH
-        $(error "Found python$(PYTHON_COMPATIBLE_VERSION) in current environment, but it should be setting as \
-            default interpreter, try to set $(PYTHON_COMPATIBLE_PATH) in PATH environment variable.")
+ifeq "$(filter $(PY_FAVORED_VER).%, $(PY_CHECK_VERSION))" ""
+    PYTHON := $(shell which "python"$(PY_FAVORED_VER))
+    ifdef PYTHON
+        $(error "Found python$(PY_FAVORED_VER), but it should be set as \
+            default interpreter, try to set $(PYTHON) in PATH environment variable.")
     else
-        $(error "$(shell python -V):Unsupported python version, only available for Python$(PYTHON_COMPATIBLE_VERSION).")
+        $(error "$(shell python -V):Unsupported python version, only available for Python$(PY_FAVORED_VER).")
     endif
 endif
 
@@ -26,5 +30,5 @@ endif
 PY_CHECK_MOD_IOTA := $(shell python -c "import iota" 2>/dev/null && \
                        echo 1 || echo 0)
 ifneq ("$(PY_CHECK_MOD_IOTA)","1")
-    $(error "Dependency error $@ because PyOTA is not installed, to install the latest version: pip3 install pyota")
+    $(error "Dependency error $@ because PyOTA is not installed, to install the latest version: pip install pyota")
 endif
