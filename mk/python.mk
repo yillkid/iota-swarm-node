0b1bbf87c6008d7bc92d9b0d6283b1cc4251d4d0
yillkid <yillkid@gmail.com>
2018-08-22 13:22:53 +0800

    Refractory sanity checks

--- mk/python.mk
+++ mk/python.mk
@@ -1,35 +1,30 @@
 # check Python version
 PYTHON := $(shell which "python")
-PYTHON_COMPATIBLE = python3.5
+PYTHON_COMPATIBLE_VERSION = 3.5
 
 ifndef PYTHON
-    $(error "Python is not executable, try to check the PATH environment variable or install $(PYTHON_COMPATIBLE) package.")
-endif # PYTHON
+    $(error "Python is not executable, try to check the PATH environment variable or \
+        install python$(PYTHON_COMPATIBLE_VERSION) package.")
+endif # $(shell which "python")
 
-ifdef PYTHON
-    PY_CHECK_VERSION := $(shell python -c "print(__import__('sys').version_info[0:2])")
+PY_CHECK_VERSION := $(shell python -c "print(str(__import__('sys').version_info.major) + \
+    str('.') + str(__import__('sys').version_info.minor) )")
 
-ifneq ($(PY_CHECK_VERSION), (3, 5))
-    PYTHON_COMPATIBLE_PATH := $(shell which $(PYTHON_COMPATIBLE))
+ifneq ($(PY_CHECK_VERSION), $(PYTHON_COMPATIBLE_VERSION))
+
+    PYTHON_COMPATIBLE_PATH := $(shell which "python"$(PYTHON_COMPATIBLE_VERSION))
 
     ifdef PYTHON_COMPATIBLE_PATH
-        $(error "Found $(PYTHON_COMPATIBLE) in current environment, but it should be setting as \
-            default interpreter, try to create a symbolic link for $(PYTHON_COMPATIBLE_PATH)")
+        $(error "Found python$(PYTHON_COMPATIBLE_VERSION) in current environment, but it should be setting as \
+            default interpreter, try to set $(PYTHON_COMPATIBLE_PATH) in PATH environment variable.")
     else
-        $(error "$(shell python -V):Invalid Python version, only available for $(PYTHON_COMPATIBLE).")
+        $(error "$(shell python -V):Unsupported python version, only available for Python$(PYTHON_COMPATIBLE_VERSION).")
     endif
 endif
-endif # PYTHON
-
-ifndef PYTHON
-
-endif # PYTHON
 
 # check "iota" module in Python installation
-ifdef PYTHON
-PY_CHECK_MOD_IOTA := $(shell $(PYTHON) -c "import iota" 2>/dev/null && \
+PY_CHECK_MOD_IOTA := $(shell python -c "import iota" 2>/dev/null && \
                        echo 1 || echo 0)
 ifneq ("$(PY_CHECK_MOD_IOTA)","1")
-    $(error "dependency error $@ because PyOTA is not installed, to install the latest version: pip3 install pyota")
+    $(error "Dependency error $@ because PyOTA is not installed, to install the latest version: pip3 install pyota")
 endif
-endif # PYTHON
