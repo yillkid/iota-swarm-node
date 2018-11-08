6a0108a2dff1fcfd1ddc9251f69d862a8102aa70
ender503 <ender503.tw@gmail.com>
2018-11-08 15:03:52 +0800

    Use Flask web framework
    To enhance the API testability, use the Flask web framework instead of
    the Python built-in HTTP server.

--- server.py
+++ server.py
@@ -1,80 +1,53 @@
 #!/usr/bin/env python
 import json
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from optparse import OptionParser
+from flask import Flask, request
+from flask_cors import CORS
 
 from swarm_node import send_transfer, get_tips, generate_address
-
 from extensions.tangleid import main as extension_tangleid
 
 PORT = 8000
 
+app = Flask(__name__)
+CORS(app)
 
-class RequestHandler(BaseHTTPRequestHandler):
+@app.route('/')
+def swarm_node_info():
+    response = {
+        'status': 'SUCCESS',
+        'data': 'Hello I am a swarm node.'
+    }
 
-    def do_OPTIONS(self):
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-        self.end_headers()
+    return json.dumps(response)
 
-    def do_GET(self):
-        response = {
-            'status': 'SUCCESS',
-            'data': 'Hello I am a swarm node.'
-        }
 
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        self.wfile.write(str(json.dumps(response)).encode('utf-8'))
+@app.route('/', methods=['POST'])
+def execute_api():
+    request_data = request.get_data()
+    print("Get request data ... %s" % (str(request_data)))
 
-    def do_POST(self):
+    request_command = json.loads(request_data)
 
-        request_path = self.path
-
-        request_headers = self.headers
-        content_length = request_headers.get("Content-length")
-        length = int(content_length) if content_length else 0
-        request_data = self.rfile.read(length)
-        request_data = request_data.decode('utf-8')
-        print("Get request data ... %s" % (str(request_data)))
-
-        request_command = json.loads(request_data)
-
-        if request_command['command'] == "generate_address":
-            result = generate_address()
-        elif request_command['command'] == "get_tips":
-            result = get_tips(int(request_command['type']))
-        elif request_command['command'] == "send_transfer":
-            if 'debug' not in request_command:
-                debug = 0
-            else:
-                debug = int(request_command['debug'])
-
-            dict_tips = get_tips(int(request_command['tips_type']))
-            result = send_transfer(
-                request_command['tag'], request_command['message'], request_command['address'], int(
-                    request_command['value']), dict_tips, debug)
+    if request_command['command'] == "generate_address":
+        result = generate_address()
+    elif request_command['command'] == "get_tips":
+        result = get_tips(int(request_command['type']))
+    elif request_command['command'] == "send_transfer":
+        if 'debug' not in request_command:
+            debug = 0
         else:
-            result = extension_tangleid.load(request_data)
+            debug = int(request_command['debug'])
 
-        print("Result ... %s" % (str(result)))
+        dict_tips = get_tips(int(request_command['tips_type']))
+        result = send_transfer(
+            request_command['tag'], request_command['message'], request_command['address'], int(
+                request_command['value']), dict_tips, debug)
+    else:
+        result = extension_tangleid.load(request_data)
 
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        self.wfile.write(str(result).encode('utf-8'))
+    print("Result ... %s" % (str(result)))
 
+    return result
 
-def http_server():
-
-    print("Listening on localhost: %s" % (str(PORT)))
-    server = HTTPServer(('', PORT), RequestHandler)
-    server.serve_forever()
-
-
-http_server()
+if __name__ == "__main__":
+    app.run(port=PORT, threaded=True)
